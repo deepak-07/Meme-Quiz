d71326bb3240b3def030a40b4f6f9ee10b6906cb
DeepakYadav <42115559+deepak-07@users.noreply.github.com>
2021-10-27 01:05:36 +0530

    body configured

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./app.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main">Main body </div>
+      <div className="pyramid">
+        Points
+        <ul className="pointList">
+          <li classNamḍe="pointListItem">400</li>
+          <li classNamḍe="pointListItem">300</li>
+          <li classNamḍe="pointListItem">200</li>
+          <li classNamḍe="pointListItem">100</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
