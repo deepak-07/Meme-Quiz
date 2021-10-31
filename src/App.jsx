3cf1565e372f6fa438e4e80fcb39143e432f68f8
DeepakYadav <42115559+deepak-07@users.noreply.github.com>
2021-10-31 15:53:53 +0530

    feat: game over functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import "./app.css";
 import Quiz from "./components/Quiz";
 
 function App() {
   const [questionNumber, setQuestionNumber] = useState(1);
-  const [timeOut, setTimeOut] = useState(false);
+  const [stop, setStop] = useState(false);
+  const [earned, setEarned] = useState("0");
 
   const data = [
     {
@@ -140,54 +141,69 @@ function App() {
       ],
     },
   ];
-  const pointPyramid = [
-    {
-      id: 1,
-      point: "10",
-    },
-    {
-      id: 2,
-      point: "20",
-    },
-    {
-      id: 3,
-      point: "30",
-    },
-    {
-      id: 4,
-      point: "40",
-    },
-    {
-      id: 5,
-      point: "50",
-    },
-    {
-      id: 6,
-      point: "60",
-    },
-    {
-      id: 7,
-      point: "70",
-    },
-    {
-      id: 8,
-      point: "80",
-    },
-  ].reverse();
+  const pointPyramid = useMemo(() => 
+    [
+      {
+        id: 1,
+        point: "10",
+      },
+      {
+        id: 2,
+        point: "20",
+      },
+      {
+        id: 3,
+        point: "30",
+      },
+      {
+        id: 4,
+        point: "40",
+      },
+      {
+        id: 5,
+        point: "50",
+      },
+      {
+        id: 6,
+        point: "60",
+      },
+      {
+        id: 7,
+        point: "70",
+      },
+      {
+        id: 8,
+        point: "80",
+      },
+    ].reverse(), []);
+
+  useEffect(() => {
+    questionNumber > 1 &&
+      setEarned(
+        pointPyramid.find((item) => item.id === questionNumber - 1).point
+      );
+  }, [pointPyramid, questionNumber]);
+
   return (
     <div className="App">
       <div className="main">
-        <div className="top">
-          <div className="timer">30</div>
-        </div>
-        <div className="bottom">
-          <Quiz
-            data={data}
-            setTimeOut={setTimeOut}
-            setQuestionNumber={setQuestionNumber}
-            questionNumber={questionNumber}
-          />
-        </div>
+        {stop ? (
+          <h1 className="endGame">Khatam Tata Goodbye Gaya! : {earned}</h1>
+        ) : (
+          <>
+            <div className="top">
+              <div className="timer">30</div>
+            </div>
+            <div className="bottom">
+              <Quiz
+                data={data}
+                setStop={setStop}
+                setQuestionNumber={setQuestionNumber}
+                questionNumber={questionNumber}
+              />
+            </div>
+          </>
+        )}
       </div>
       <div className="pyramid">
         <ul className="pointList">
@@ -197,7 +213,8 @@ function App() {
                 questionNumber === item.id
                   ? "pointListItem active"
                   : "pointListItem"
-              } key={item.id}
+              }
+              key={item.id}
             >
               <span className="pointListItemNumber">{item.id}</span>
               <span className="pointListItemPoints">{item.point}</span>
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,22 +1,53 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
-export default function Quiz(
+export default function Quiz({
   data,
-  setTimeout,
+  setStop,
   setQuestionNumber,
-  questionNumber
-) {
+  questionNumber,
+}) {
   const [question, setQuestion] = useState(null);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [className, setClassName] = useState("answer");
+
+  const delay = (duration, callback) => {
+    setTimeout(() => {
+      callback();
+    }, duration);
+  };
+
+  const handleClick = (item) => {
+    setSelectedAnswer(item);
+    setClassName("answer active");
+    delay(500, () =>
+      setClassName(item.correct ? "answer correct" : "answer wrong")
+    );
+    delay(3500, () => {
+      if (item.correct) {
+        setQuestionNumber((prev) => prev + 1);
+        setSelectedAnswer(null);
+      } else {
+        setStop(true);
+      }
+    });
+  };
 
   useEffect(() => {
     setQuestion(data[questionNumber - 1]);
   }, [data, questionNumber]);
+
   return (
     <div className="quiz">
       <div className="question">{question?.question}</div>
       <div className="answers">
         {question?.answers.map((item) => (
-          <div className="answer" key={item.text}>{item.text}</div>
+          <div
+            className={selectedAnswer === item ? className : "answer"}
+            onClick={() => handleClick(item)}
+            key={item.text}
+          >
+            {item.text}
+          </div>
         ))}
       </div>
     </div>
